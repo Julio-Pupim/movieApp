80ef33415f8ff304317b06f1edebd47a70139a9a
julio.pupim <julio.minellamatheus@gmail.com>
2023-10-02 15:38:55 -0300

    fix: encondig API

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 
-const APILINK = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&page=1`;
+const APILINK = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${TOKEN_API}&page=1`;
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&query=";
+const SEARCHAPI = `https://api.themoviedb.org/3/search/movie?&api_key=${TOKEN_API}&query=`;
 
 const main = document.getElementById("section");
 const form = document.getElementById("form");
